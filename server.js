e84be6c119587a9382ad944489fe3a66927e3fbb
Karl Kompatscher <karl.kompatscher.03@gmail.com>
2025-01-27 09:26:32 +0100

    Use cors and web-vitals

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const firebaseAdmin = require('firebase-admin');
+const cors = require('cors');
+const path = require('path');
 const { createNewGame } = require('./gameLogic');
 require('dotenv').config();
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('..wattland-f4776-firebase-adminsdk-fbsvc-2023737c5c.json');
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount)
-});
+const serviceAccount = require('../wattland-f4776-firebase-adminsdk-fbsvc-2023737c5c.json');
+if (!firebaseAdmin.apps.length) {
+    console.log('Initializing Firebase app...');
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(serviceAccount),
+    });
+} else {
+    console.log('Using existing Firebase app...');
+    firebaseAdmin.app(); // If already initialized, reuse the existing instance
+}
 
 const db = firebaseAdmin.firestore();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());  // Allow all origins (for development purposes)
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 // Define basic routes for the API
